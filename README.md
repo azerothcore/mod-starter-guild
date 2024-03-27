74ec76c276fb8c0e1f4a879d68044f35f3a69fa8
Walter Pagani <paganiwalter@gmail.com>
2024-03-27 01:52:03 -0300

    chore. Add files to the module (#15)
    * chore. Add files to the module
    
    * Add example SQL in README file

--- README.md
+++ README.md
@@ -1,26 +1,39 @@
 # ![logo](https://raw.githubusercontent.com/azerothcore/azerothcore.github.io/master/images/logo-github.png) AzerothCore
+
 ## Starter Guild
-- Latest build status with azerothcore: [![Build Status](https://github.com/azerothcore/mod-starter-guild/workflows/core-build/badge.svg?branch=master&event=push)](https://github.com/azerothcore/mod-starter-guild)
+
+- Latest build status with azerothcore:
+
+[![Build Status](https://github.com/azerothcore/mod-starter-guild/workflows/core-build/badge.svg?branch=master&event=push)](https://github.com/azerothcore/mod-starter-guild)
 
 ## Description
 
 This module automatically assigns new characters to a specific guild depending on the faction.
 
-
 ## Usage
 
-- Create a new guild ingame for each faction with `.guild create` or directly into the database
-- Edit the configuration file
-- Create a new character and open the guild tab
+- Create a new guild ingame for each faction with `.guild create` or directly into the database.
+- Edit the configuration file.
+- Create a new character and open the guild tab.
+- Keep in mind that the limit of players is 1000 per guild.
 
+## SQL example to create the guild
+
+- Check that the ids do not exist, and in this case, the owner will be the first creating player, generally the administrator.
+
+```sql
+DELETE FROM `guild` WHERE `guildid` IN (1, 2);
+INSERT INTO `guild` (`guildid`, `name`, `leaderguid`, `motd`) VALUES
+(1, "Horde Starting Guild", 1, "Lok'Tar Ogar!"),
+(2, "Alliance Starting Guild", 1, "Fight together, for the alliance");
+```
 
 ## Credits
 
-* [stygiancore](  http://stygianthebest.github.io ): (Author of the module):
-* [SoulSeekkor]( https://github.com/SoulSeekkor ) cleaned up deprecation warnings and fixed naming
+* [stygiancore](  http://stygianthebest.github.io ): (Author of the module).
+* [SoulSeekkor]( https://github.com/SoulSeekkor ) cleaned up deprecation warnings and fixed naming.
 
 * AzerothCore: [repository](https://github.com/azerothcore) - [website](http://azerothcore.org/) - [discord chat community](https://discord.gg/PaqQRkd)
-
 
 ## License
 
---
+++ src/mod_starterguild.h
@@ -0,0 +1,79 @@
+/*
+
+# Starter Guild #
+
+#### A module for AzerothCore (https://github.com/azerothcore/mod-starter-guild)
+#### Originally by [StygianTheBest](https://github.com/StygianTheBest/AzerothCore-Content/tree/master/Modules)
+------------------------------------------------------------------------------------------------------------------
+
+
+### Description ###
+------------------------------------------------------------------------------------------------------------------
+This module automatically joins new players to a guild of your choice on first login.
+
+
+### Data ###
+------------------------------------------------------------------------------------------------------------------
+- Type: Player/Server
+- Script: StarterGuild
+- Config: Yes
+    - Enable Module
+    - Enable Module Announce
+    - Set ID for Horde and Alliance Guilds
+- SQL: No
+
+
+### Version ###
+------------------------------------------------------------------------------------------------------------------
+- v2022.04.09 - Fixed deprecation warnings and renamed files/updated comments
+- v2017.08.01
+- v2017.08.02 - Fix crash prevention when no guilds exist
+
+
+### Credits ###
+------------------------------------------------------------------------------------------------------------------
+- [Blizzard Entertainment](http://blizzard.com)
+- [TrinityCore](https://github.com/TrinityCore/TrinityCore/blob/3.3.5/THANKS)
+- [SunwellCore](http://www.azerothcore.org/pages/sunwell.pl/)
+- [AzerothCore](https://github.com/AzerothCore/azerothcore-wotlk/graphs/contributors)
+- [AzerothCore Discord](https://discord.gg/gkt4y2x)
+- [EMUDevs](https://youtube.com/user/EmuDevs)
+- [AC-Web](http://ac-web.org/)
+- [ModCraft.io](http://modcraft.io/)
+- [OwnedCore](http://ownedcore.com/)
+- [OregonCore](https://wiki.oregon-core.net/)
+- [Wowhead.com](http://wowhead.com)
+- [AoWoW](https://wotlk.evowow.com/)
+- [SoulSeekkor](https://github.com/SoulSeekkor)
+
+
+### License ###
+------------------------------------------------------------------------------------------------------------------
+- This code and content is released under the [GNU AGPL v3](https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3).
+
+*/
+
+
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "GuildMgr.h"
+#include "Configuration/Config.h"
+#include "Chat.h"
+
+#define Welcome_Name "Notice"
+uint32 guildMemberCount = 1000;
+
+class StarterGuild : public PlayerScript
+{
+public:
+    StarterGuild() : PlayerScript("StarterGuild") { }
+
+    void OnLogin(Player* player) override;
+    void OnFirstLogin(Player* player) override;
+    void addPlayerToGuild(Player* player);
+};
+
+void AddStarterGuildScripts()
+{
+    new StarterGuild();
+}
